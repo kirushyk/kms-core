21b9db52513208cec140d725138d6cc8c6e3b5cd
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-02-28 20:57:58 +0200

    mingw32: Link against pre-generated sources

--- i686-w64-mingw32.mk
+++ i686-w64-mingw32.mk
@@ -9,31 +9,33 @@ CFLAGS= \
 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/gstreamer-1.0/include \
 -I/usr/i686-w64-mingw32/sys-root/mingw/include/glib-2.0 \
 -I/usr/i686-w64-mingw32/sys-root/mingw/lib/glib-2.0/include \
+-I./win32
 
 SDPAGENT_TARGET=libkmssdpagent.dll
 
 SDPAGENT_SRC= \
-src/gst-plugins/commons/sdpagent/kmssdpmidext.c \
-src/gst-plugins/commons/sdpagent/kmssdpconnectionext.c \
-src/gst-plugins/commons/sdpagent/kmssdpgroupmanager.c \
-src/gst-plugins/commons/sdpagent/kmssdpmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdpagent.c \
-src/gst-plugins/commons/sdpagent/kmssdpcontext.c \
-src/gst-plugins/commons/sdpagent/kmsisdpmediaextension.c \
-src/gst-plugins/commons/sdpagent/kmssdpagentcommon.c \
-src/gst-plugins/commons/sdpagent/kmssdpbundlegroup.c \
-src/gst-plugins/commons/sdpagent/kmssdprtpsavpmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdprtpmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdpbasegroup.c \
-src/gst-plugins/commons/sdpagent/kmssdprtpsavpfmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmsisdpsessionextension.c \
-src/gst-plugins/commons/sdpagent/kmssdprtpavpfmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdppayloadmanager.c \
-src/gst-plugins/commons/sdpagent/kmssdpsctpmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdprtpavpmediahandler.c \
-src/gst-plugins/commons/sdpagent/kmssdpsdesext.c \
-src/gst-plugins/commons/sdpagent/kmsisdppayloadmanager.c \
-src/gst-plugins/commons/sdpagent/kmssdprejectmediahandler.c
+./src/gst-plugins/commons/sdpagent/kmssdpmidext.c \
+./src/gst-plugins/commons/sdpagent/kmssdpconnectionext.c \
+./src/gst-plugins/commons/sdpagent/kmssdpgroupmanager.c \
+./src/gst-plugins/commons/sdpagent/kmssdpmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdpagent.c \
+./src/gst-plugins/commons/sdpagent/kmssdpcontext.c \
+./src/gst-plugins/commons/sdpagent/kmsisdpmediaextension.c \
+./src/gst-plugins/commons/sdpagent/kmssdpagentcommon.c \
+./src/gst-plugins/commons/sdpagent/kmssdpbundlegroup.c \
+./src/gst-plugins/commons/sdpagent/kmssdprtpsavpmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdprtpmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdpbasegroup.c \
+./src/gst-plugins/commons/sdpagent/kmssdprtpsavpfmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmsisdpsessionextension.c \
+./src/gst-plugins/commons/sdpagent/kmssdprtpavpfmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdppayloadmanager.c \
+./src/gst-plugins/commons/sdpagent/kmssdpsctpmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdprtpavpmediahandler.c \
+./src/gst-plugins/commons/sdpagent/kmssdpsdesext.c \
+./src/gst-plugins/commons/sdpagent/kmsisdppayloadmanager.c \
+./src/gst-plugins/commons/sdpagent/kmssdprejectmediahandler.c \
+./win32/kms-sdp-agent-marshal.c
 
 SDPAGENT_LIBS= \
 -L/usr/i686-w64-mingw32/sys-root/mingw/lib \
