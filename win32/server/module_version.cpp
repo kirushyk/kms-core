d8b83aee01116dd71d1b3bfe1fb1c9729a18e5d2
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-03-04 22:23:05 +0200

    Update to 6.4.1-6

--- win32/server/module_version.cpp
+++ win32/server/module_version.cpp
@@ -1,3 +1,3 @@
 
     extern "C" {const char * getModuleVersion ();}
-    const char * getModuleVersion () {return "6.4.1~1.g63ea327";}
+    const char * getModuleVersion () {return "6.4.1~6.gf33eb2d";}
