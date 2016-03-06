ea2b8b7d5016f19707d23782a32e79e1023d63d3
Kyrylo Polezhaiev <polezhaiev@ukr.net>
2016-03-07 01:27:23 +0200

    mingw32: Add target for KMSCOREINTERFACE static library

--- i686-w64-mingw32.mk
+++ i686-w64-mingw32.mk
@@ -1,5 +1,7 @@
+AR=i686-w64-mingw32-ar
 CC=i686-w64-mingw32-gcc
 CXX=i686-w64-mingw32-g++
+RANLIB=i686-w64-mingw32-ranlib
 TARGET_DIR=./build/
 
 CXXFLAGS=--std=gnu++17 -fpermissive
@@ -119,10 +121,101 @@ KMSCOMMONS_LIBS= \
 -lrpcrt4 \
 -lole32
 
+KMSCOREINTERFACE_TARGET=libkmscoreinterface.a
+
+KMSCOREINTERFACE_SRC= \
+./win32/server/interface/generated-cpp/MediaObjectInternal.cpp \
+./win32/server/interface/generated-cpp/ServerManagerInternal.cpp \
+./win32/server/interface/generated-cpp/SessionEndpointInternal.cpp \
+./win32/server/interface/generated-cpp/HubInternal.cpp \
+./win32/server/interface/generated-cpp/FilterInternal.cpp \
+./win32/server/interface/generated-cpp/EndpointInternal.cpp \
+./win32/server/interface/generated-cpp/HubPortInternal.cpp \
+./win32/server/interface/generated-cpp/PassThroughInternal.cpp \
+./win32/server/interface/generated-cpp/UriEndpointInternal.cpp \
+./win32/server/interface/generated-cpp/MediaPipelineInternal.cpp \
+./win32/server/interface/generated-cpp/SdpEndpointInternal.cpp \
+./win32/server/interface/generated-cpp/BaseRtpEndpointInternal.cpp \
+./win32/server/interface/generated-cpp/MediaElementInternal.cpp \
+./win32/server/interface/generated-cpp/MediaObject.cpp \
+./win32/server/interface/generated-cpp/ServerManager.cpp \
+./win32/server/interface/generated-cpp/SessionEndpoint.cpp \
+./win32/server/interface/generated-cpp/Hub.cpp \
+./win32/server/interface/generated-cpp/Filter.cpp \
+./win32/server/interface/generated-cpp/Endpoint.cpp \
+./win32/server/interface/generated-cpp/HubPort.cpp \
+./win32/server/interface/generated-cpp/PassThrough.cpp \
+./win32/server/interface/generated-cpp/UriEndpoint.cpp \
+./win32/server/interface/generated-cpp/MediaPipeline.cpp \
+./win32/server/interface/generated-cpp/SdpEndpoint.cpp \
+./win32/server/interface/generated-cpp/BaseRtpEndpoint.cpp \
+./win32/server/interface/generated-cpp/MediaElement.cpp \
+./win32/server/interface/generated-cpp/RaiseBase.cpp \
+./win32/server/interface/generated-cpp/Error.cpp \
+./win32/server/interface/generated-cpp/MediaSessionTerminated.cpp \
+./win32/server/interface/generated-cpp/MediaSessionStarted.cpp \
+./win32/server/interface/generated-cpp/Media.cpp \
+./win32/server/interface/generated-cpp/ObjectCreated.cpp \
+./win32/server/interface/generated-cpp/ObjectDestroyed.cpp \
+./win32/server/interface/generated-cpp/MediaStateChanged.cpp \
+./win32/server/interface/generated-cpp/ConnectionStateChanged.cpp \
+./win32/server/interface/generated-cpp/MediaFlowOutStateChange.cpp \
+./win32/server/interface/generated-cpp/MediaFlowInStateChange.cpp \
+./win32/server/interface/generated-cpp/ElementConnected.cpp \
+./win32/server/interface/generated-cpp/ElementDisconnected.cpp \
+./win32/server/interface/generated-cpp/ServerInfo.cpp \
+./win32/server/interface/generated-cpp/ServerType.cpp \
+./win32/server/interface/generated-cpp/GstreamerDotDetails.cpp \
+./win32/server/interface/generated-cpp/ModuleInfo.cpp \
+./win32/server/interface/generated-cpp/MediaState.cpp \
+./win32/server/interface/generated-cpp/MediaFlowState.cpp \
+./win32/server/interface/generated-cpp/ConnectionState.cpp \
+./win32/server/interface/generated-cpp/MediaType.cpp \
+./win32/server/interface/generated-cpp/FilterType.cpp \
+./win32/server/interface/generated-cpp/VideoCodec.cpp \
+./win32/server/interface/generated-cpp/AudioCodec.cpp \
+./win32/server/interface/generated-cpp/Fraction.cpp \
+./win32/server/interface/generated-cpp/AudioCaps.cpp \
+./win32/server/interface/generated-cpp/VideoCaps.cpp \
+./win32/server/interface/generated-cpp/ElementConnectionData.cpp \
+./win32/server/interface/generated-cpp/Tag.cpp \
+./win32/server/interface/generated-cpp/StatsType.cpp \
+./win32/server/interface/generated-cpp/MediaLatencyStat.cpp \
+./win32/server/interface/generated-cpp/Stats.cpp \
+./win32/server/interface/generated-cpp/ElementStats.cpp \
+./win32/server/interface/generated-cpp/EndpointStats.cpp \
+./win32/server/interface/generated-cpp/RTCStats.cpp \
+./win32/server/interface/generated-cpp/RTCRTPStreamStats.cpp \
+./win32/server/interface/generated-cpp/RTCCodec.cpp \
+./win32/server/interface/generated-cpp/RTCInboundRTPStreamStats.cpp \
+./win32/server/interface/generated-cpp/RTCOutboundRTPStreamStats.cpp \
+./win32/server/interface/generated-cpp/RTCPeerConnectionStats.cpp \
+./win32/server/interface/generated-cpp/RTCMediaStreamStats.cpp \
+./win32/server/interface/generated-cpp/RTCMediaStreamTrackStats.cpp \
+./win32/server/interface/generated-cpp/RTCDataChannelState.cpp \
+./win32/server/interface/generated-cpp/RTCDataChannelStats.cpp \
+./win32/server/interface/generated-cpp/RTCTransportStats.cpp \
+./win32/server/interface/generated-cpp/RTCStatsIceCandidateType.cpp \
+./win32/server/interface/generated-cpp/RTCIceCandidateAttributes.cpp \
+./win32/server/interface/generated-cpp/RTCStatsIceCandidatePairState.cpp \
+./win32/server/interface/generated-cpp/RTCIceCandidatePairStats.cpp \
+./win32/server/interface/generated-cpp/RTCCertificateStats.cpp \
+./win32/server/interface/generated-cpp/CodecConfiguration.cpp \
+./win32/server/interface/generated-cpp/RembParams.cpp
+
+KMSCOREINTERFACE_LIBS= \
+-L/usr/i686-w64-mingw32/sys-root/mingw/lib \
+-L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
+-L/usr/i686-w64-mingw32/lib/ \
+-L../kms-jsonrpc/build/ \
+-L../jsoncpp/build/ \
+-lkmsjsoncpp.dll \
+-lkmsjsonrpc.dll
+
 KMSCOREIMPL_TARGET=libkmscoreimpl.dll
 
 KMSCOREIMPL_SRC= \
-./src/server/implementation/DotGraph.cpp \
+#./src/server/implementation/DotGraph.cpp \
 ./src/server/implementation/RegisterParent.cpp \
 ./src/server/implementation/objects/ServerManagerImpl.cpp \
 ./src/server/implementation/objects/PassThroughImpl.cpp \
@@ -142,8 +235,41 @@ KMSCOREIMPL_SRC= \
 ./src/server/implementation/ModuleManager.cpp \
 ./src/server/implementation/WorkerPool.cpp \
 ./src/server/implementation/UUIDGenerator.cpp \
-./src/server/implementation/EventHandler.cpp \
-\
+./src/server/implementation/EventHandler.cpp
+
+KMSCOREIMPL_LIBS= \
+-L/usr/i686-w64-mingw32/sys-root/mingw/lib \
+-L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
+-L/usr/i686-w64-mingw32/lib/ \
+-L../kms-jsonrpc/build/ \
+-L../jsoncpp/build/ \
+-L./build/ \
+-lsigc-2.0 \
+-lkmsjsoncpp.dll \
+-lkmsjsonrpc.dll \
+-lgstreamer-1.0 \
+-lws2_32 \
+-lgstsdp-1.0.dll \
+-lglibmm-2.4 \
+-lgobject-2.0 \
+-lglib-2.0 \
+-lkmssdpagent \
+-lkmsgstcommons \
+-lboost_system-mt \
+-lboost_log-mt \
+-lboost_log_setup-mt \
+-lboost_program_options-mt \
+-lboost_filesystem-mt \
+-lboost_thread-mt
+
+KMSCOREMODULE_TARGET=libkmscoremodule.dll
+
+KMSCOREMODULE_SRC= \
+#./win32/server/implementation/generated-cpp/Module.cpp \
+./win32/server/module_generation_time.cpp \
+./win32/server/module_version.cpp \
+./win32/server/module_name.cpp \
+./win32/server/module_descriptor.cpp \
 ./win32/server/implementation/generated-cpp/SessionEndpointImplInternal.cpp \
 ./win32/server/implementation/generated-cpp/MediaElementImplInternal.cpp \
 ./win32/server/implementation/generated-cpp/MediaPipelineImplInternal.cpp \
@@ -236,37 +362,12 @@ KMSCOREIMPL_SRC= \
 ./win32/server/interface/generated-cpp/VideoCaps.cpp \
 ./win32/server/interface/generated-cpp/BaseRtpEndpoint.cpp \
 ./win32/server/interface/generated-cpp/MediaSessionStarted.cpp
-#./win32/server/module_version.cpp \
-#./win32/server/module_name.cpp \
-#./win32/server/module_generation_time.cpp
-#./win32/server/module_descriptor.cpp \
-#./src/server/implementation/objects/MediaPadImpl.cpp \
-#./win32/server/implementation/generated-cpp/Module.cpp \
 
-KMSCOREIMPL_LIBS= \
+KMSCOREMODULE_LIBS= \
 -L/usr/i686-w64-mingw32/sys-root/mingw/lib \
 -L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
--L/usr/i686-w64-mingw32/lib/ \
--L../kms-jsonrpc/build/ \
--L../jsoncpp/build/ \
--L./build/ \
--lsigc-2.0 \
--lkmsjsoncpp.dll \
--lkmsjsonrpc.dll \
--lgstreamer-1.0 \
--lws2_32 \
--lgstsdp-1.0.dll \
--lglibmm-2.4 \
--lgobject-2.0 \
--lglib-2.0 \
--lkmssdpagent \
--lkmsgstcommons \
--lboost_system-mt \
--lboost_log-mt \
--lboost_log_setup-mt \
--lboost_program_options-mt \
--lboost_filesystem-mt \
--lboost_thread-mt
+-L/usr/i686-w64-mingw32/lib/
+
 
 KMSCOREPLUGINS_TARGET=libkmscoreplugins.dll
 
@@ -313,28 +414,22 @@ VP8PARSE_LIBS= \
 -lgobject-2.0 \
 -lglib-2.0
 
-KMSCOREMODULE_TARGET=libkmscoremodule.dll
-
-KMSCOREMODULE_SRC= \
-./win32/server/implementation/generated-cpp/Module.cpp \
-./win32/server/module_generation_time.cpp \
-./win32/server/module_version.cpp \
-./win32/server/module_name.cpp \
-./win32/server/module_descriptor.cpp
-
-KMSCOREMODULE_LIBS= \
--L/usr/i686-w64-mingw32/sys-root/mingw/lib \
--L/usr/lib/gcc/i686-w64-mingw32/5.2.0 \
--L/usr/i686-w64-mingw32/lib/
-
-
 SDPAGENT_OBJS=$(SDPAGENT_SRC:.c=.o)
 KMSCOMMONS_OBJS=$(KMSCOMMONS_SRC:.c=.o)
+KMSCOREINTERFACE_OBJS=$(KMSCOREINTERFACE_SRC:.cpp=.o)
 KMSCOREIMPL_OBJS=$(KMSCOREIMPL_SRC:.cpp=.o)
 KMSCOREMODULE_OBJS=$(KMSCOREMODULE_SRC:.cpp=.o)
 KMSCOREPLUGINS_OBJS=$(KMSCOREPLUGINS_SRC:.c=.o)
 
-all: $(TARGET_DIR)/$(SDPAGENT_TARGET) $(TARGET_DIR)/$(KMSCOMMONS_TARGET) $(TARGET_DIR)/$(KMSCOREMODULE_TARGET) $(TARGET_DIR)/$(KMSCOREIMPL_TARGET) $(TARGET_DIR)/$(KMSCOREPLUGINS_TARGET) $(TARGET_DIR)/$(VP8PARSE_TARGET)
+all: \
+$(TARGET_DIR)/$(SDPAGENT_TARGET) \
+$(TARGET_DIR)/$(KMSCOMMONS_TARGET) \
+$(TARGET_DIR)/$(KMSCOREINTERFACE_TARGET)
+
+#$(TARGET_DIR)/$(KMSCOREMODULE_TARGET) \
+#$(TARGET_DIR)/$(KMSCOREIMPL_TARGET) \
+#$(TARGET_DIR)/$(KMSCOREPLUGINS_TARGET) \
+#$(TARGET_DIR)/$(VP8PARSE_TARGET)
 
 $(TARGET_DIR)/$(SDPAGENT_TARGET): $(SDPAGENT_OBJS)
 	mkdir -p $(TARGET_DIR)
@@ -343,6 +438,11 @@ $(TARGET_DIR)/$(SDPAGENT_TARGET): $(SDPAGENT_OBJS)
 $(TARGET_DIR)/$(KMSCOMMONS_TARGET): $(KMSCOMMONS_OBJS)
 	mkdir -p $(TARGET_DIR)
 	$(CC) -shared -o $(TARGET_DIR)/$(KMSCOMMONS_TARGET) $(CFLAGS) $(KMSCOMMONS_OBJS) $(KMSCOMMONS_LIBS) -Wl,--out-implib,$(TARGET_DIR)/$(KMSCOMMONS_TARGET).a
+
+$(TARGET_DIR)/$(KMSCOREINTERFACE_TARGET): $(KMSCOREINTERFACE_OBJS)
+	mkdir -p $(TARGET_DIR)
+	$(AR) cr $(TARGET_DIR)/$(KMSCOREINTERFACE_TARGET) $(KMSCOREINTERFACE_OBJS)
+	$(RANLIB) $(TARGET_DIR)/$(KMSCOREINTERFACE_TARGET)
 
 $(TARGET_DIR)/$(KMSCOREIMPL_TARGET): $(KMSCOREIMPL_OBJS)
 	mkdir -p $(TARGET_DIR)
